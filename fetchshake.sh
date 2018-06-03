3821aaef45c82d995e153d34e8969d84ffc86324
Glenn McKechnie <glennmckechnie@gmail.com>
2018-06-03 14:13:58 +1000

    finish revert to single

--- fetchshake.sh
+++ fetchshake.sh
@@ -7,7 +7,10 @@
 
 
 login='user_name@machine_address'
-ind_html='/tmp/myshake.inc'
+login='myshake@192.168.1.4'
+ind_html='index.htm'
+stat_id_1='AM'
+stat_id_2='RC98F'
 www_myshake='/var/www/html/weewx/myshake/'
 Utc=$(date -u)
 Local=$(date)
@@ -28,8 +31,8 @@ fi
  echo "<h2>MyShake: seismology for the masses!</h2>
   <p><a href=\"https://raspberryshake.org/about/\">Raspberryshake.org</a> is a project that encourages and assists with the deployment of personal seismographs.<br> These seismographs use the raspberrypi mini computer to monitor an attached digitizer and a super sensitive motion sensor(s).
   </p><p>
-  The network consists of many stations scattered throughout <a href=\"https://raspberryshake.net/stationview/\"> the world</a> that do their part to contribute to the data, and even the detection, of the activity that occurs on <a href=\"https://raspberryshake.net/eqview/\">this active planet.</a></p>
-  <p>The following is the most recent helicorder image for the detector at this location - <a href=\"https://raspberryshake.net/stationview/#?net=AM&sta=RC98F\">AM.RC98F</a><br>
+  The network consists of many stations scattered throughout <a href=\"https://raspberryshake.net/stationview/\"> the world</a> that do their part to contribute to the data, and even the detection, of the seismic activity that occurs on <a href=\"https://raspberryshake.net/eqview/\">this planet.</a></p>
+  <p>The following is the most recent helicorder image for the detector at this location - <a href=\"https://raspberryshake.net/stationview/#?net=$stat_id_1&sta=$stat_id_2\">$stat_id_1.$stat_id_2</a><br>
   </p><p>
   The time is recorded as UTC time ( $Utc ), not as local time ( $Local ).<br>The resulting image is updated every 2 minutes, although there will be a time lag as it needs to be fetched from the unit first.<br><br> Click on an image to further enlarge it.</p>" > $ind_html
 
@@ -39,8 +42,7 @@ for i in `find -L . -mount -depth -maxdepth 1 -type f ! -name 'ind*'`
  do
   if [ $flag -eq 1 ]
   then
-    echo `basename $i` | sed "s/^.*/<a href=\"&\">$style<\/a>/" >> $ind_html
-    # weewx specific # echo "</div><div id=\"history_week\" class=\"plot_container\" style=\"display:none\"><p>Archive of previous Helicorder results, click on an entry to load an image for the corresponding time period.</p>" >> $ind_html
+    echo `basename $i` | sed "s/^.*/<a href=\"&\">$style<\/a><hr><h2>Archives<\/h2>/" >> $ind_html
     flag=0
   else
     base_str=$(echo `basename $i` | cut -d'.' -f2)
