b53aaf388c593dcfe1a3d2eeaf68683bea9aa9ca
Glenn McKechnie <glennmckechnie@gmail.com>
2018-06-03 14:03:11 +1000

    revert to simple

--- fetchshake.sh
+++ fetchshake.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-# use crontab to fetch every 2 minutes
+# use this crontab to fetch images only, every 2 minutes
+# */2 * * * * bash /home/pi/git-masterofpis/myshake/fetchshake.sh
+# or use this entry to fetch ALL the data, twice daily (consider it a backup script)
+# */1 0,12  * * * bash /home/pi/git-masterofpis/myshake/fetchshake.sh all
 
-login='myshake@192.168.1.4'
-ind_html='index.html'
+
+login='user_name@machine_address'
+ind_html='/tmp/myshake.inc'
 www_myshake='/var/www/html/weewx/myshake/'
 Utc=$(date -u)
 Local=$(date)
@@ -13,32 +17,15 @@ if [ $# -eq 1 ]
  then
   # we want to fetch all the data
   # chmod 0777 /opt/shake/
-  rsync -av $login:/opt/data /opt/shake/
+  rsync -a $login:/opt/data /opt/shake/
 else
   # or we just want to fetch the image
   # chmod 0777 /var/www/html/weewx/myshake
   cd $www_myshake
-  rsync -av $login:/opt/data/gifs//RC98F_EHZ_AM_00*.gif $www_myshake
+  rsync -a $login:/opt/data/gifs/RC98F_EHZ_AM_00*.gif $www_myshake
 fi
 
-echo "<!doctype html>
-<html lang=\"en\">
-  <head>
-   <title>MyShake - seismology for the masses!</title>
-   <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
-    <link rel=\"icon\" type=\"image/png\" href=\"../favicon.ico\" />
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"../seasons.css\"/>
-    <script src=\"../standard.js\"></script>
-  </head>
- <body onload=\"setup();\">
- <div id=\"title_bar\">
-  <div id=\"title\">
-    <h1 class=\"page_title\">Messmate Farm, Broomfield, Victoria</h1>
-  </div>
-</div>
-<div id=\"contents\">
-   <p><a href=\"../index.html\">&#10096; Current Conditions</a></p>
-  <h1>MyShake: seismology for the masses!</h1>
+ echo "<h2>MyShake: seismology for the masses!</h2>
   <p><a href=\"https://raspberryshake.org/about/\">Raspberryshake.org</a> is a project that encourages and assists with the deployment of personal seismographs.<br> These seismographs use the raspberrypi mini computer to monitor an attached digitizer and a super sensitive motion sensor(s).
   </p><p>
   The network consists of many stations scattered throughout <a href=\"https://raspberryshake.net/stationview/\"> the world</a> that do their part to contribute to the data, and even the detection, of the activity that occurs on <a href=\"https://raspberryshake.net/eqview/\">this active planet.</a></p>
@@ -52,7 +39,8 @@ for i in `find -L . -mount -depth -maxdepth 1 -type f ! -name 'ind*'`
  do
   if [ $flag -eq 1 ]
   then
-    echo `basename $i` | sed "s/^.*/<a href=\"&\">$style<\/a><hr><h2>Archives<\/h2>/" >> $ind_html
+    echo `basename $i` | sed "s/^.*/<a href=\"&\">$style<\/a>/" >> $ind_html
+    # weewx specific # echo "</div><div id=\"history_week\" class=\"plot_container\" style=\"display:none\"><p>Archive of previous Helicorder results, click on an entry to load an image for the corresponding time period.</p>" >> $ind_html
     flag=0
   else
     base_str=$(echo `basename $i` | cut -d'.' -f2)
@@ -60,9 +48,9 @@ for i in `find -L . -mount -depth -maxdepth 1 -type f ! -name 'ind*'`
     HH=$(echo  $base_str | awk '{print substr($0,9,3)}')
     fdate=`date -d $YMD +'%Y-%m-%d'`
     # text and image
-    echo `basename $i` | sed "s/^.*/<a href=\"&\">$fdate ($HH)<br> $style2<br>/" >> $ind_html
+    #echo `basename $i` | sed "s/^.*/<a href=\"&\">$fdate ($HH)<br> $style2<br>/" >> $ind_html
     # text only
-    #echo `basename $i` | sed "s/^.*/<a href=\"&\">$fdate ($HH)<\/a><br>/" >> $ind_html
+    echo `basename $i` | sed "s/^.*/<a href=\"&\">$fdate ($HH)<\/a><br>/" >> $ind_html
   fi
 done
-echo '<br><hr></div></body></html>' >> $ind_html
+echo '<br>' >> $ind_html
